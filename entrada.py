943cbf204114990b85f177de0ecc10b881b1f20e
Your Name <you@example.com>
2026-02-12 11:12:56 -0300

    aula 1

---
+++ entrada.py
@@ -0,0 +1,17 @@
+nome = input ('Qual o seu nome?')
+
+print('Ola ',nome, ', seja bem vindo ao meu mundo')
+
+
+
+nome = input('Qual o seu nome?')
+senha = input('Qual sua senha?')
+dominio = input('Qual o seu dominio?')
+print('Ola', nome, 'seja bem vindo ao meu mundo')
+print('Sua senha é: ', senha)
+print('O seu dominio é ', dominio)
+
+email = nome + '@' + dominio
+print('Seu e-mail é: ', email)
+
+
